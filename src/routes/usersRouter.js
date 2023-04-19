90a53481070a947d6a22cc8ba8b6e60f18963c8a
Kirill Emanov <Emanov1993@yandex.ru>
2023-04-20 01:04:48 +0800

    Готов функционал админки

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,9 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../views/Users');
 const renderTemplate = require('../lib/renderTemplate');
+const { User } = require('../../db/models');
 
 router.get('/', async (req, res) => {
-  renderTemplate(Users, {}, res, req);
+  try {
+    const userList = await User.findAll({
+      attributes: ['name', 'role'],
+      raw: true,
+    });
+
+    renderTemplate(Users, { userList }, res, req);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
+router.put('/', async (req, res) => {
+  try {
+    const { name, role } = req.body;
+    await User.update({ role }, { where: { name } });
+
+    const updatedUser = await User.findOne({
+      where: { name },
+      attributes: ['name', 'role'],
+      raw: true,
+    });
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+
 module.exports = router;
+
--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -1,12 +1,23 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Users({ userSession }) {
-  // const { user } = userSession;
+module.exports = function Users({ userSession, userList }) {
   return (
     <Layout userSession={userSession}>
+      <script defer src="js/users.js" />
       <div className="usersContainer">
         <h1>Пользователи</h1>
+        <ul>
+          {userList.map((user) => (
+            <li key={user.name}>
+              <span className='name'>{user.name}</span> - <span className='role'>{user.role ? 'Администратор' : 'Пользователь'}</span>
+              <select defaultValue={user.role ? 'true' : 'false'}>
+                <option value="false">Пользователь</option>
+                <option value="true">Администратор</option>
+              </select>
+            </li>
+          ))}
+        </ul>
       </div>
     </Layout>
   );
