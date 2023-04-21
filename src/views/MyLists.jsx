6deb4cc526dffdfec9c39cb697d2171abf42c66d
SurferSam <surfeursam@gmail.com>
2023-04-21 13:32:08 +0300

    Стили для моих листов

--- src/views/MyLists.jsx
+++ src/views/MyLists.jsx
@@ -7,29 +7,36 @@ module.exports = function MyLists({ userSession, MyCoopers }) {
     <Layout userSession={userSession}>
        <script src="/js/my.lists.js" defer />
       <div className="listContainer">
-        <h1>Мои листки адаптации</h1>
+        <h1 className='myListsAdapt'>Мои листки адаптации</h1><br /><br />
         <form name='createShablon'>
-        <a type='button' className='createShablonBtn' href='/mylists/shablon'>Создать листок</a>
-        </form>
-        <ol>
+          {/* <div className='createShablonDiv'> */}
+          <a type='button' className='createShablonBtn createShablonDiv' href='/mylists/shablon'>Создать листок</a>
+          {/* </div> */}
+        
+        </form><br /><br /><br />
+        <ol className='employeeList'>
         {MyCoopers.map((el) => 
-            <li>
+            
+            <li >
             {el.employee}
             {" "}
+            <div className='oneEmployeeList'>
             <form name='ShowShablon'>
-            <button className='ShowListBtn' id={el.id}>Посмотреть листок</button>
+            <button className='ShowListBtn ListBtn' id={el.id}>Посмотреть листок</button>
             </form>
             <form name='EditShablon'>
-            <button className='EditListBtn' id={el.id}>Отправить листок</button>
+            <button className='EditListBtn ListBtn' id={el.id}>Отправить листок</button>
             </form>
             <form name='DeleteShablon'>
-            <button className='DeleteListBtn' data-shablonid={el.id} id={el.id}>Удалить листок</button>
+            <button className='DeleteListBtn ListBtn' data-shablonid={el.id} id={el.id}>Удалить листок</button>
             </form>
+            </div><br />
             </li>
           )}
         </ol>
     
       </div>
+      
     </Layout>
   );
 };
