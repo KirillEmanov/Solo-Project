45c2bb6c5ce803f61d0cd0c4b98c0b01eaceb629
SurferSam <surfeursam@gmail.com>
2023-04-21 16:23:21 +0300

    Finalochka

--- src/routes/edit.shablon.router.js
+++ src/routes/edit.shablon.router.js
@@ -21,7 +21,7 @@ router.put('/edit.shablon/:id', async (req, res) => {
   try {
     // console.log(req.body);
     const shablonEdit = await Shablon.findOne({ where: { id: req.params.id } });
-    const { task1, task2, task3, task4, task5, task8 } = req.body;
+    const { task1, task2, task3, task4, task5, task6, task7, task8, task9, task10, task11, task12 } = req.body;
     shablonEdit.task1 = task1;
     console.log("🚀🚀🚀🚀🚀 ~ shablonEdit:", shablonEdit)
     
@@ -29,65 +29,18 @@ router.put('/edit.shablon/:id', async (req, res) => {
     shablonEdit.task3 = task3;
     shablonEdit.task4 = task4;
     shablonEdit.task5 = task5;
+    shablonEdit.task6 = task6;
+    shablonEdit.task7 = task7;
     shablonEdit.task8 = task8;
+    shablonEdit.task9 = task9;
+    shablonEdit.task10 = task10;
+    shablonEdit.task11 = task11;
+    shablonEdit.task12 = task12;
     shablonEdit.save();
     res.sendStatus(200)
   } catch (error) {
     console.error(error);
   }
 });
-
-
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const user = await User.findOne({ where: { name: req.session.user }, raw: true });
-//     const userId = user.id;
-//     const MyCoopers = await Shablon.findAll({ where: { userId }, raw: true })
-//     renderTemplate(MyLists, { MyCoopers }, res, req);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// router.get('/edit.shablon', async (req, res) => {
-//   renderTemplate(Shablons, {}, res, req);
-// });
-
-// router.post('/shablon', async (req, res) => {
-//     try {
-//         const { task1, task2, task3, task4, task5, employee, userId} = req.body;
-//         const newShablon = await Shablon.create({ task1, task2, task3, task4, task5, employee, userId });
-//         res.json(newShablon)
-//     } catch (error){
-//         console.log(error)
-//         res.send(error)
-//     }
-// })
-
-
-
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     await Shablon.destroy({ where: { id } });
-//     // res.sendStatus(200);
-//   } catch (error) {
-//     console.log(error);
-//     res.send(error);
-//   }
-//   return res.redirect('/mylists');
-// });
-
-
-
-// router.delete('/entry/:id', async (req, res) => {
-//   try {
-//     await Entry.destroy({ where: { id: req.params.id } });
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   return res.redirect('/all-the-entries');
-// });
 
 module.exports = router;
